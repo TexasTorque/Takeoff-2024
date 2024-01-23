caaea0e705f4fe81792958948ddc6b784bc90011
Juicestus <jus@justusl.com>
2024-01-22 19:28:20 -0600

    Perception changes

--- src/main/java/org/texastorque/subsystems/Perception.java
+++ src/main/java/org/texastorque/subsystems/Perception.java
@@ -1,16 +1,18 @@
 package org.texastorque.subsystems;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.littletonrobotics.junction.Logger;
 import org.texastorque.Debug;
 import org.texastorque.Field;
 import org.texastorque.Subsystems;
 import org.texastorque.toast.lib.Camera;
 import org.texastorque.toast.lib.Toast;
+import org.texastorque.toast.lib.Util;
 import org.texastorque.toast.lib.pipelines.AprilTags;
 import org.texastorque.toast.lib.pipelines.AprilTags.AprilTagDetection;
-import org.texastorque.toast.lib.thirdparty.BetterPoseEstimator;
-import org.texastorque.toast.lib.thirdparty.BetterPoseEstimator.TimestampedVisionUpdate;
 import org.texastorque.torquelib.base.TorqueMode;
 import org.texastorque.torquelib.base.TorqueState;
 import org.texastorque.torquelib.base.TorqueStatorSubsystem;
@@ -23,35 +25,53 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Twist2d;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 
+/**
+ * Robot perception subsystem, handles sensors that the robot uses
+ * to contextualize it's surroundings. 
+ * 
+ * 
+ * Other perception subsystems are below for reference:
+ * - https://github.com/TexasTorque/Bravo-2024/blob/21ab2550dfbc8c3d1c73813b9feec5aa52dac205/src/main/java/org/texastorque/subsystems/Perception.java
+ * - https://github.com/TexasTorque/Banana-2024/blob/0d04f9470ef629f2e7fc1128fed3fcafd0b1d2bf/src/main/java/org/texastorque/subsystems/Perception.java
+ * 
+ * @author Justus 
+ */
 public final class Perception extends TorqueStatorSubsystem<Perception.State> implements Subsystems {
 
-    public enum State implements TorqueState {
-        VISION;
-    }
+    public enum State implements TorqueState { VISION; }
+    
+     /**
+     * Standard deviations of model states. Increase these numbers to trust your
+     * model's state estimates less. This matrix is in the form [x, y, theta]ᵀ,
+     * with units in meters and radians, then meters.
+     */
+    private static final Vector<N3> ODOMETRY_STDS = VecBuilder.fill(.05, .05, Units.degreesToRadians(2));
 
-    private static final Vector<N3> INV_KINEMATICS_STDS = VecBuilder.fill(.05, .05, Units.degreesToRadians(2));
+    /**
+     * Standard deviations of the vision measurements. Increase these numbers to
+     * trust global measurements from vision less. This matrix is in the form
+     * [x, y, theta]ᵀ, with units in meters and radians.
+     */
+
     private static final Vector<N3> VISION_STDS = VecBuilder.fill(.1, .1, Units.degreesToRadians(5));
 
-    private static final double MAX_ANGULAR_VELOCITY = Math.PI / 2;
-    private static final double MAX_DISTANCE = 4;
+    /**
+     * The maximum angular velocity of the robot (in radians per second) and maximum distance from
+     * the camera to the april tag (in meters) where we trust the vision measurements. 
+     */
+    private static final double MAX_ANGULAR_VELOCITY_RADS = Math.PI * 2, MAX_DISTANCE = 6;
+
 
     private final Toast toast;
-
     private final SwerveDrivePoseEstimator poseEstimator;
-    private final BetterPoseEstimator poseEstimatorMech;
 
     private final TorqueNavXGyro gyro = TorqueNavXGyro.getInstance();
-    public final Field2d fieldWpi = new Field2d(), fieldMech = new Field2d();
+    public final Field2d field = new Field2d();
     private AprilTagFieldLayout fieldMap = Field.getFieldLayout();
-
-    private SwerveModulePosition[] lastModulePositions;
-    private Rotation2d lastHeading;
 
     public Perception() {
         super(State.VISION);
@@ -61,76 +81,41 @@ public final class Perception extends TorqueStatorSubsystem<Perception.State> im
                 drivebase.kinematics,
                 getHeading(),
                 drivebase.getModulePositions(),
-                new Pose2d(), INV_KINEMATICS_STDS, VISION_STDS);
+                new Pose2d(), ODOMETRY_STDS, VISION_STDS);
 
-        poseEstimatorMech = new BetterPoseEstimator(INV_KINEMATICS_STDS);
-
+        // Add toast cameras 
         toast.addCamera(new Camera("sim", new Transform3d()));
 
+        // Register the apriltags pipeline
         toast.iterCams(cam -> cam.addPipeline(new AprilTags(cam.id)));
 
-        Debug.field("Field (WPI)", fieldWpi);
-        Debug.field("Field (Mech)", fieldMech);
-
-        lastModulePositions = drivebase.getModulePositions();
-        lastHeading = getHeading();
+        // Log the field map to the dashboard 
+        Debug.field("Field", field);
     }
 
     @Override
-    public void initialize(TorqueMode mode) {
-    }
-
-    public double getDistanceToTarget() {
-        return poseEstimator.getEstimatedPosition().getTranslation().getNorm();
-    }
+    public void initialize(final TorqueMode mode) { }
 
     @Override
-    public void update(TorqueMode mode) {
-        updateInvKinematics();
-        updateVision();
+    public void update(final TorqueMode mode) {
+        updateOdometryLocalization();
+        updateVisionLocalization();
 
-        fieldWpi.setRobotPose(poseEstimator.getEstimatedPosition());
-        fieldMech.setRobotPose(poseEstimatorMech.getLatestPose());
+        field.setRobotPose(poseEstimator.getEstimatedPosition());
 
-        Debug.log("Pose (WPI)", pose2d2str(poseEstimator.getEstimatedPosition()));
-        Debug.log("Pose (Mech)", pose2d2str(poseEstimatorMech.getLatestPose()));
+        Debug.log("Pose", Util.pose2d2str(poseEstimator.getEstimatedPosition()));
         Debug.log("Heading (°)", getHeading().getDegrees());
     }
 
-    public double compressedTimestamp() {
-        return (((double) System.currentTimeMillis()) / 1000d) % 10000d;
-    }
-
-    public double compressedTimestamp(final double seconds) {
-        return seconds % 10000d;
-    }
-
-    public void updateInvKinematics() {
+    public void updateOdometryLocalization() {
+        // Updates the pose estimator with swerve encoder feedback
         poseEstimator.update(getHeading(), drivebase.getModulePositions());
-
-        // Calculate delta positions
-        final SwerveModulePosition[] wheelDeltas = new SwerveModulePosition[4], modules = drivebase.getModulePositions();
-        for (int i = 0; i < 4; i++) {
-            wheelDeltas[i] = new SwerveModulePosition(modules[i].distanceMeters - lastModulePositions[i].distanceMeters,
-                    modules[i].angle);
-            lastModulePositions[i] = modules[i];
-        }
-
-        // Calculate twist
-        final Twist2d twist = drivebase.kinematics.toTwist2d(wheelDeltas);
-        final Rotation2d heading = getHeading();
-        final Twist2d deltaTwist = new Twist2d(twist.dx, twist.dy, heading.minus(lastHeading).getRadians());
-        lastHeading = heading;
-
-        // Send drivebase data to poseEstimator
-        poseEstimatorMech.addDriveData(compressedTimestamp(), deltaTwist);
     }
 
-    private final List<TimestampedVisionUpdate> visionUpdates = new ArrayList<>();
+    private final Map<Integer, Pose3d> tagsInView = new HashMap<>(); 
 
-    public void updateVision() {
-        // Update the toast class
-        toast.update();
+    public void updateVisionLocalization() {
+        toast.update(); // Updates all the vision pipelines.
 
         toast.iterCams((cam) -> {
             final var pipeOpt = cam.getPipeline(AprilTags.class);
@@ -139,8 +124,6 @@ public final class Perception extends TorqueStatorSubsystem<Perception.State> im
             final AprilTags pipe = pipeOpt.get();
 
             final List<AprilTagDetection> detections = pipe.getDetections();
-
-            visionUpdates.clear();
 
             for (final AprilTagDetection detection : detections) {
 
@@ -152,13 +135,15 @@ public final class Perception extends TorqueStatorSubsystem<Perception.State> im
                 // then we ignore the detection and move on
                 if (!detection.isValidDetection()
                         || !Field.isIDValid(detection.id)
-                        || Math.abs(gyro.getAngularVelocity().getRadians()) > MAX_ANGULAR_VELOCITY
+                        || Math.abs(gyro.getAngularVelocity().getRadians()) > MAX_ANGULAR_VELOCITY_RADS
                         || detection.getDistance() > MAX_DISTANCE)
                     continue;
 
-
                 // get tag pose in world space
                 final Pose3d tagPose = fieldMap.getTagPose(detection.id).get(); // should never fail
+
+                // we see this tag so we add it to the tagsInView map. this is so we can log the detections on advantagescope
+                if (!tagsInView.containsKey(detection.id)) tagsInView.put(detection.id, tagPose);
 
                 // converting from cam space to robot space by adding the camera->tag transform with the center->camera transform
                 final Transform3d robotSpaceTransform = detection.transform.plus(cam.transform);    
@@ -172,47 +157,56 @@ public final class Perception extends TorqueStatorSubsystem<Perception.State> im
                 if (!Field.isPoseOnField(estPose))
                     continue;
 
+                // add the processed vision messurement to the pose estimator
                 poseEstimator.addVisionMeasurement(estPose, detection.timestamp);
-
-                visionUpdates.add(
-                        new TimestampedVisionUpdate(compressedTimestamp(detection.timestamp), estPose, VISION_STDS));
             }
-
-            poseEstimatorMech.addVisionData(visionUpdates);
         });
+
+        // Serializes and pushes the seen tags to networktables so we can view detections on advantagescop
+        Logger.recordOutput("Perception/TagPoses", tagsInView.values().toArray(new Pose3d[tagsInView.values().size()]));
     }
 
+    /**
+     * Gyro heading (yaw, CCW around the Z-axis) as a Rotation2d.
+     */
     public Rotation2d getHeading() {
         return gyro.getHeadingCCW();
     }
 
+    /**
+     * Gyro angluar velocity (yaw, CCW around the Z-axis) as a Rotation2d.
+     * Represents the angle (radians or degrees) per second.
+     */
     public Rotation2d getAngularVelocity() {
         return gyro.getAngularVelocity();
     }
 
+    /**
+     * Tear the gyro, make the current heading "north" (0° yaw).
+     */
     public void resetGyro() {
         gyro.setOffsetCW(Rotation2d.fromRadians(0));
     }
 
-    public void resetPose() {
-        poseEstimator.resetPosition(getHeading(), drivebase.getModulePositions(), new Pose2d());
-        poseEstimatorMech.resetPose(new Pose2d());
-    }
-
-    private String pose2d2str(final Pose2d pose) {
-        return String.format("(%.2fm, %.2fm) @ %.2f°", pose.getX(), pose.getY(), pose.getRotation().getDegrees());
-    }
-
+     /**
+     * Returns the current perceived position of the robot.
+     */
     public Pose2d getPose() {
         return poseEstimator.getEstimatedPosition();
     }
 
-    public void setPose(Pose2d pose) {
+    /**
+     * Resets the current position of the robot to the argument.
+     */
+    public void setPose(final Pose2d pose) {
         poseEstimator.resetPosition(getHeading(), drivebase.getModulePositions(), pose);
     }
 
-    public void setGoalPose(Pose2d pose) {
-        fieldWpi.getObject("goalPose").setPose(pose);
+    /**
+     * Reset the position in the pose estimator to be at the origin.
+     */
+    public void resetPose() {
+        setPose(new Pose2d());
     }
 
     private static volatile Perception instance;
