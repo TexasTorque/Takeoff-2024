55e6ababbb4baaf48a310ef9ba7b398b7f1c8445
0marA <theengineermachine@gmail.com>
2024-03-30 13:24:27 -0500

    made src to 40 path

--- src/main/java/org/texastorque/auto/AutoManager.java
+++ src/main/java/org/texastorque/auto/AutoManager.java
@@ -102,6 +102,7 @@ public final class AutoManager extends TorqueAutoManager implements Subsystems {
         addBaseAuto(StartPoint.CTR, NotePoint.N_2, NotePoint.N_3, NotePoint.N_50);
         addBaseAuto(StartPoint.SRC, NotePoint.N_3, NotePoint.N_50);
         addBaseAuto(StartPoint.SRC, NotePoint.N_50, NotePoint.N_40);
+        addBaseAuto(StartPoint.SRC, NotePoint.N_40);
     }
 
     /** Create a base auto and come up with a name for it */
